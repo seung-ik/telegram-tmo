b033a7e58e261d01c8374149e1450caecbe2b562
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-06-05 09:31:16 +0900

    feat: user name data 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import "@twa-dev/sdk";
+import { Telegram } from "@twa-dev/types";
+
+declare global {
+  interface Window {
+    Telegram: Telegram;
+  }
+}
+
 import { useNavigate } from "react-router-dom";
 import Router from "./Router";
 
 function App() {
   const navigate = useNavigate();
+  console.log(window.Telegram.WebApp.initDataUnsafe.user?.id, "info");
 
   return (
     <div className="App">
@@ -16,6 +25,10 @@ function App() {
           border: "2px solid white",
         }}
       >
+        <div>{window.Telegram.WebApp.initDataUnsafe.user?.id || "id"}</div>
+        <div>
+          {window.Telegram.WebApp.initDataUnsafe.user?.username || "username"}
+        </div>
         <div onClick={() => navigate("/")}>튜터리얼</div>
         <div onClick={() => navigate("/game")}>게임</div>
       </div>
