6b06111a58c3fd05154971cf17bb9464858817f7
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-06-20 16:22:03 +0900

    feat: receive address 추가

--- src/DashBoard.tsx
+++ src/DashBoard.tsx
@@ -9,6 +9,7 @@ import styles from "./dashboard.module.scss";
 import classNames from "classnames";
 import { toNano } from "@ton/core";
 import { beginCell } from "@ton/ton";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
@@ -16,21 +17,21 @@ const DashBoard = () => {
   const { connected } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
   const wallet = useTonWallet();
+  const [recieveAddress, setRecieveAddress] = useState("");
   const [tonConnectUI] = useTonConnectUI();
 
   const body = beginCell()
     .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
-    .storeStringTail("?(testnet-현재지갑)에서 phone 지갑으로 전송 테스트") // write our text comment
+    .storeStringTail("전송테스트") // write our text comment
     .endCell();
 
   console.log(wallet, "연결된집갑");
-  const k = tonConnectUI.getWallets();
 
   const myTransaction = {
     validUntil: Math.floor(Date.now() / 1000) + 360,
     messages: [
       {
-        address: "0QA2fleBSz891dZ8CqSiq7LOR5CZ9jQMKV8U2dzGqCZbiiUi", // destination address
+        address: recieveAddress, // destination address
         amount: toNano(0.05).toString(), //Toncoin in nanotons
         payload: body.toBoc().toString("base64"), // payload with comment in body
       },
@@ -70,18 +71,29 @@ const DashBoard = () => {
         </a>
         <div>
           {connected && (
-            <button
-              style={{
-                border: "2px solid green",
-                padding: "10px",
-                cursor: "pointer",
-                background: "gray",
-                color: "white",
-              }}
-              onClick={() => tonConnectUI.sendTransaction(myTransaction)}
-            >
-              phone 지갑 으로 0.5ton 전송 버튼
-            </button>
+            <>
+              <span>받는주소</span>
+              <input
+                type="text"
+                value={recieveAddress}
+                onChange={(e) => setRecieveAddress(e.target.value)}
+              />
+              <button
+                style={{
+                  border: "2px solid green",
+                  padding: "10px",
+                  cursor: "pointer",
+                  background: "gray",
+                  color: "white",
+                }}
+                onClick={() => {
+                  if (!recieveAddress) return;
+                  tonConnectUI.sendTransaction(myTransaction);
+                }}
+              >
+                0.05ton 전송 버튼
+              </button>
+            </>
           )}
         </div>
       </div>
