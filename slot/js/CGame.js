33ec6586018d1d72a8f5a8a46ba2e4c9ffc10761
seung-ik <69374857+seung-ik@users.noreply.github.com>
2024-07-03 00:29:16 +0000

    Deploying to gh-pages from @ seung-ik/telegram-tmo@a1f0d7b1c6bcdbc9f8ad135a98185539d80fe89b 🚀

--- slot/js/CGame.js
+++ slot/js/CGame.js
@@ -1,273 +1,259 @@
-function CGame(oData){
-    
-    var _bInitGame;
-    var _bPlayerWins;
-    
-    var _iTimeIdle;
-    var _iTimeWin;
-    var _iCurAnim;
-    var _iGameState;
-    var _iCurWin;
-    var _iAdCounter;
-    var _iNumSpin;
+function CGame(oData) {
+  var _bInitGame;
+  var _bPlayerWins;
 
-    var _aProbability;
+  var _iTimeIdle;
+  var _iTimeWin;
+  var _iCurAnim;
+  var _iGameState;
+  var _iCurWin;
+  var _iAdCounter;
+  var _iNumSpin;
 
-    var _oInterface;
-    var _oEndPanel = null;
-    var _oParent;
-    var _oWheelContainer;
-    var _oWheel;
-    var _oLeds;
-    
-    var _oDebug;
-    
-    this._init = function(){
-        _bPlayerWins = false;
-     
-        _iTimeIdle = 0;
-        _iTimeWin = 0;
-        _iCurWin = -1;        
-        _iGameState = STATE_IDLE;
-        _iAdCounter = 0;
-        _iNumSpin = NUM_INSTANT_SPIN;
+  var _aProbability;
 
-        var oBg = createBitmap(s_oSpriteLibrary.getSprite('bg_game'));
-        s_oStage.addChild(oBg);
+  var _oInterface;
+  var _oEndPanel = null;
+  var _oParent;
+  var _oWheelContainer;
+  var _oWheel;
+  var _oLeds;
 
-        _aProbability = new Array();
+  var _oDebug;
 
-        _bInitGame=true;
-        
-        _oWheelContainer = new createjs.Container();
-        s_oStage.addChild(_oWheelContainer);
+  this._init = function () {
+    _bPlayerWins = false;
 
-        this.attachWheel();
-        
-        _oInterface = new CInterface(); 
+    _iTimeIdle = 0;
+    _iTimeWin = 0;
+    _iCurWin = -1;
+    _iGameState = STATE_IDLE;
+    _iAdCounter = 0;
+    _iNumSpin = NUM_INSTANT_SPIN;
 
-        new CHelpPanel();
+    var oBg = createBitmap(s_oSpriteLibrary.getSprite("bg_game"));
+    s_oStage.addChild(oBg);
 
-        this._initProbability();
-        
-    };
-    
-    this.attachWheel = function(){
-        var pCenterWheel = {x: CANVAS_WIDTH/2 + 140, y: CANVAS_HEIGHT/2+30};
-        s_oWheel.attachWheel(pCenterWheel.x, pCenterWheel.y, _oWheelContainer);
-        
-        _oLeds = new CLeds(pCenterWheel.x, pCenterWheel.y, _oWheelContainer);
-        _iCurAnim = _oLeds.getState();
-        
-        _oWheel = s_oWheel;
-    };
-    
-    this._initProbability = function(){
-       
-        var aPrizeLength = new Array();
+    _aProbability = new Array();
 
-        for(var i=0; i<INSTANT_WHEEL_SETTINGS.length; i++){
-            aPrizeLength[i] = INSTANT_WHEEL_SETTINGS[i].win_occurrence;
+    _bInitGame = true;
+
+    _oWheelContainer = new createjs.Container();
+    s_oStage.addChild(_oWheelContainer);
+
+    this.attachWheel();
+
+    _oInterface = new CInterface();
+
+    new CHelpPanel();
+
+    this._initProbability();
+  };
+
+  this.attachWheel = function () {
+    var pCenterWheel = { x: CANVAS_WIDTH / 2 + 140, y: CANVAS_HEIGHT / 2 + 30 };
+    s_oWheel.attachWheel(pCenterWheel.x, pCenterWheel.y, _oWheelContainer);
+
+    _oLeds = new CLeds(pCenterWheel.x, pCenterWheel.y, _oWheelContainer);
+    _iCurAnim = _oLeds.getState();
+
+    _oWheel = s_oWheel;
+  };
+
+  this._initProbability = function () {
+    var aPrizeLength = new Array();
+
+    for (var i = 0; i < INSTANT_WHEEL_SETTINGS.length; i++) {
+      aPrizeLength[i] = INSTANT_WHEEL_SETTINGS[i].win_occurrence;
+    }
+
+    for (var i = 0; i < aPrizeLength.length; i++) {
+      for (var j = 0; j < aPrizeLength[i]; j++) {
+        _aProbability.push(i);
+      }
+    }
+  };
+
+  this.tryShowAd = function () {
+    _iAdCounter++;
+    if (_iAdCounter === AD_SHOW_COUNTER) {
+      _iAdCounter = 0;
+      $(s_oMain).trigger("show_interlevel_ad");
+    }
+  };
+
+  this._getInstantWinPrize = function () {
+    var iPrizeToChoose =
+      _aProbability[Math.floor(Math.random() * _aProbability.length)];
+
+    return iPrizeToChoose;
+  };
+
+  this.spinWheel = function () {
+    _oInterface.disableSpin(true);
+    _iGameState = STATE_SPIN;
+    _iTimeWin = 0;
+
+    _iNumSpin--;
+    _oInterface.refreshNumSpin(_iNumSpin);
+    _iCurWin = this._getInstantWinPrize();
+
+    //SPIN
+    _oWheel.spin(_iCurWin);
+
+    playSound("start_reel", 1, false);
+    playSound("reel", 0.2, true);
+  };
+
+  this.releaseWheel = function () {
+    this.tryShowAd();
+    _oInterface.disableSpin(false);
+
+    stopSound("reel");
+
+    _iGameState = STATE_LOSE;
+
+    if (INSTANT_WHEEL_SETTINGS[_iCurWin].prizewinning) {
+      _iGameState = STATE_WIN;
+      this._instantWinPanel();
+
+      playSound("win", 1, false);
+    } else {
+      playSound("game_over", 1, false);
+
+      if (_iNumSpin === 0) {
+        this._instantLosePanel();
+      }
+    }
+  };
+
+  this._instantWinPanel = function () {
+    $(s_oMain).trigger("save_score", _iCurWin);
+
+    var oInstantWinPanel = new CInstantWinPanel(_iCurWin);
+    var oSprite = "image_" + _iCurWin;
+    oInstantWinPanel.show(oSprite);
+  };
+
+  this._instantLosePanel = function () {
+    $(s_oMain).trigger("save_score", _iCurWin);
+
+    var oInstantLosePanel = new CInstantLosePanel(_iCurWin);
+    var oSprite = "image_" + _iCurWin;
+    oInstantLosePanel.show(oSprite);
+  };
+
+  this.unload = function () {
+    _bInitGame = false;
+
+    _oInterface.unload();
+    if (_oEndPanel !== null) {
+      _oEndPanel.unload();
+    }
+    s_oWheel.unload();
+    createjs.Tween.removeAllTweens();
+    s_oStage.removeAllChildren();
+
+    stopSound("reel");
+  };
+
+  this.onExit = function () {
+    $(s_oMain).trigger("end_session");
+    $(s_oMain).trigger("share_event", _iCurWin);
+
+    this.unload();
+    s_oMain.gotoMenu();
+  };
+
+  this._animLedIdle = function () {
+    _iTimeIdle += s_iTimeElaps;
+
+    if (_iTimeIdle > TIME_ANIM_IDLE) {
+      _iTimeIdle = 0;
+
+      var iRandAnim = Math.floor(Math.random() * _oLeds.getNumAnim());
+
+      while (iRandAnim === _iCurAnim) {
+        iRandAnim = Math.floor(Math.random() * _oLeds.getNumAnim());
+      }
+      _oLeds.changeAnim(iRandAnim);
+      _iCurAnim = iRandAnim;
+    }
+  };
+
+  this._animLedSpin = function () {
+    _oLeds.changeAnim(LED_SPIN);
+    _iGameState = -1;
+  };
+
+  this._animLedWin = function () {
+    if (_iTimeWin === 0) {
+      var iRandomWinAnim = LED_SPIN + 1 + Math.round(Math.random());
+      _oLeds.changeAnim(iRandomWinAnim);
+    } else if (_iTimeWin > TIME_ANIM_WIN) {
+      _iTimeIdle = TIME_ANIM_IDLE;
+      _iGameState = STATE_IDLE;
+
+      _iTimeWin = 0;
+    }
+    _iTimeWin += s_iTimeElaps;
+  };
+
+  this._animLedLose = function () {
+    if (_iTimeWin === 0) {
+      _oLeds.changeAnim(7);
+    } else if (_iTimeWin > TIME_ANIM_LOSE) {
+      _iTimeIdle = TIME_ANIM_IDLE;
+      _iGameState = STATE_IDLE;
+
+      _iTimeWin = 0;
+    }
+    _iTimeWin += s_iTimeElaps;
+  };
+
+  this.startUpdate = function () {
+    _bInitGame = true;
+  };
+
+  this.stopUpdate = function () {
+    _bInitGame = false;
+  };
+
+  this.update = function () {
+    if (_bInitGame) {
+      _oLeds.update();
+
+      switch (_iGameState) {
+        case STATE_IDLE: {
+          this._animLedIdle();
+          break;
         }
-
-        for(var i=0; i<aPrizeLength.length; i++){
-            for(var j=0; j<aPrizeLength[i]; j++){
-                _aProbability.push(i);
-            }            
-        }            
-    };
-
-    this.tryShowAd = function(){
-        _iAdCounter++;
-        if(_iAdCounter === AD_SHOW_COUNTER){
-            _iAdCounter = 0;
-            $(s_oMain).trigger("show_interlevel_ad");
+        case STATE_SPIN: {
+          this._animLedSpin();
+          break;
         }
-    };
-    
-    this._getInstantWinPrize = function(){
-        var iPrizeToChoose = _aProbability[Math.floor(Math.random()*_aProbability.length)];      
-        
-        return iPrizeToChoose;
-    };
-    
-    this.spinWheel = function(){
-        _oInterface.disableSpin(true);
-        _iGameState = STATE_SPIN;
-        _iTimeWin = 0;
-
-        _iNumSpin--;
-        _oInterface.refreshNumSpin(_iNumSpin);
-        _iCurWin = this._getInstantWinPrize();
-
-        //SPIN
-        _oWheel.spin(_iCurWin);
-        
-        playSound("start_reel",1,false);
-        playSound("reel",0.2,true);
-    };                 
-    
-    this.releaseWheel = function(){
-        this.tryShowAd();
-        _oInterface.disableSpin(false);
-        
-        stopSound("reel");
-        
-        _iGameState = STATE_LOSE;
-
-        if(INSTANT_WHEEL_SETTINGS[_iCurWin].prizewinning){
-            _iGameState = STATE_WIN;
-            this._instantWinPanel();
-            
-            playSound("win",1,false);
-        } else{
-            playSound("game_over",1,false);
-            
-            if(_iNumSpin === 0){
-                this._instantLosePanel();
-            }
+        case STATE_WIN: {
+          this._animLedWin();
+          break;
         }
-    };
-    
-    this._instantWinPanel = function(){
-        $(s_oMain).trigger("save_score",_iCurWin);
-        
-        var oInstantWinPanel = new CInstantWinPanel(_iCurWin);
-        var oSprite = "image_"+_iCurWin;
-        oInstantWinPanel.show(oSprite);
-    };
-    
-    this._instantLosePanel = function(){
-        $(s_oMain).trigger("save_score",_iCurWin);
-        
-        var oInstantLosePanel = new CInstantLosePanel(_iCurWin);
-        var oSprite = "image_"+_iCurWin;
-        oInstantLosePanel.show(oSprite);
-    };
-    
-    this.unload = function(){
-        
-        _bInitGame = false;
-        
-        _oInterface.unload();
-        if(_oEndPanel !== null){
-            _oEndPanel.unload();
+        case STATE_LOSE: {
+          this._animLedLose();
+          break;
         }
-        s_oWheel.unload();
-        createjs.Tween.removeAllTweens();
-        s_oStage.removeAllChildren();
-        
-        stopSound("reel");
-    };
- 
-    this.onExit = function(){
-        $(s_oMain).trigger("end_session");
-        $(s_oMain).trigger("share_event",_iCurWin);
-        
-        this.unload();
-        s_oMain.gotoMenu();
-        
-    };
-    
-    this._animLedIdle = function(){
-        
-        _iTimeIdle += s_iTimeElaps;
-        
-        if(_iTimeIdle > TIME_ANIM_IDLE){
-            _iTimeIdle=0;
+      }
 
-            var iRandAnim = Math.floor(Math.random()*_oLeds.getNumAnim());
-    
-            while(iRandAnim === _iCurAnim){
-                iRandAnim = Math.floor(Math.random()*_oLeds.getNumAnim());
-            }    
-            _oLeds.changeAnim(iRandAnim);
-            _iCurAnim = iRandAnim;
-        }
-    };    
-    
-    this._animLedSpin = function(){
-        _oLeds.changeAnim(LED_SPIN);
-        _iGameState =-1;
-    };
-    
-    this._animLedWin = function(){
-       
-        if(_iTimeWin === 0){
-            var iRandomWinAnim = LED_SPIN + 1 + Math.round(Math.random())
-            _oLeds.changeAnim(iRandomWinAnim);
-          
-        } else if(_iTimeWin > TIME_ANIM_WIN) {
-            _iTimeIdle = TIME_ANIM_IDLE; 
-            _iGameState = STATE_IDLE;
+      _oWheel.update();
+    }
+  };
 
-            _iTimeWin =0;
-        }
-        _iTimeWin += s_iTimeElaps;
-        
-    };
-    
-    this._animLedLose = function(){
-       
-        if(_iTimeWin === 0){            
-            _oLeds.changeAnim(7);
-         
-        } else if(_iTimeWin > TIME_ANIM_LOSE) {
-            _iTimeIdle = TIME_ANIM_IDLE; 
-            _iGameState = STATE_IDLE;
+  s_oGame = this;
 
-            _iTimeWin =0;
-        }
-        _iTimeWin += s_iTimeElaps;
-        
-    };
+  WHEEL_SPIN_TIME = oData.wheel_spin_time;
+  NUM_INSTANT_SPIN = oData.num_instant_spin;
+  AD_SHOW_COUNTER = oData.ad_show_counter;
 
-    this.startUpdate = function(){
-        _bInitGame = true;
-    };
-    
-    this.stopUpdate = function(){
-        _bInitGame = false;
-    };
-    
-    this.update = function(){
-	if(_bInitGame){
-            
-            _oLeds.update();
-          
-        
-            switch(_iGameState) {
-                case STATE_IDLE:{
-                        this._animLedIdle();
-                   break;
-                } case STATE_SPIN: {
-                        this._animLedSpin();
-                   break;              
-
-                } case STATE_WIN: {
-                        this._animLedWin();
-                   break;                             
-                } case STATE_LOSE: {
-                        this._animLedLose();
-                   break;                             
-                }    
-
-            }
-            
-            _oWheel.update();
-            
-        }
-    };
-
-    s_oGame=this;
-
-    WHEEL_SPIN_TIME = oData.wheel_spin_time;
-    NUM_INSTANT_SPIN = oData.num_instant_spin;
-    AD_SHOW_COUNTER = oData.ad_show_counter;
-
-	
-    _oParent=this;
-    this._init();
+  _oParent = this;
+  this._init();
 }
 
 var s_oGame;
--- slot/js/CInstantLosePanel.js
+++ slot/js/CInstantLosePanel.js
@@ -1,78 +1,89 @@
-function CInstantLosePanel(iCurWin){
-    
-    var _oBg;
-    var _oFade;
-    var _oGroup;
+function CInstantLosePanel(iCurWin) {
+  var _oBg;
+  var _oFade;
+  var _oGroup;
 
-    var _oMsgText;
-    var _oListener;
-    
-    this._init = function(iCurWin){
-        _oGroup = new createjs.Container();
-        _oGroup.alpha = 0;
-        _oGroup.visible=false;
-        s_oStage.addChild(_oGroup);
-       
-        
-        _oFade = new createjs.Shape();
-        _oFade.graphics.beginFill("rgba(0,0,0,1)").drawRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-        _oFade.alpha = 0;
-        _oGroup.addChild(_oFade);
-        createjs.Tween.get(_oFade).to({alpha:0.7}, 500);
-        
-        var oSprite = s_oSpriteLibrary.getSprite('msg_box');
-        _oBg = createBitmap(oSprite);
-        _oBg.x = CANVAS_WIDTH/2;
-        _oBg.y = CANVAS_HEIGHT/2;
-        _oBg.regX = oSprite.width/2;
-        _oBg.regY = oSprite.height/2;
-         _oGroup.addChild( _oBg);
+  var _oMsgText;
+  var _oListener;
 
-        _oMsgText = new CTLText(_oGroup, 
-                    CANVAS_WIDTH/2-250, (CANVAS_HEIGHT/2), 500, 100, 
-                    50, "center", "#fff", THIRD_FONT, 1,
-                    0, 0,
-                    " ",
-                    true, true, true,
-                    false );
- 
-    };
-    
-    this.unload = function(){
-        _oGroup.off("click",_oListener);
-    };
-    
-    this._initListener = function(){
-        _oListener = _oGroup.on("click",this._onExit);
-    };
-    
-    this.show = function(oSprite){
-        
-        $(s_oMain).trigger("show_interlevel_ad");
-        
-        _oMsgText.refreshText(TEXT_INSTANT_LOSE);
-        
-        _oGroup.visible = true;
-        
-        var oParent = this;
-        createjs.Tween.get(_oGroup).to({alpha:1 }, 500).call(function() {oParent._initListener();});
+  this._init = function (iCurWin) {
+    _oGroup = new createjs.Container();
+    _oGroup.alpha = 0;
+    _oGroup.visible = false;
+    s_oStage.addChild(_oGroup);
 
-    };
+    _oFade = new createjs.Shape();
+    _oFade.graphics
+      .beginFill("rgba(0,0,0,1)")
+      .drawRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    _oFade.alpha = 0;
+    _oGroup.addChild(_oFade);
+    createjs.Tween.get(_oFade).to({ alpha: 0.7 }, 500);
 
-    
-    this._onExit = function(){
-        
-        if(INSTANT_WHEEL_SETTINGS[iCurWin].redeem_link !== ""){
-            window.open(INSTANT_WHEEL_SETTINGS[iCurWin].redeem_link);
-        }
-        
-        _oGroup.off("click",_oListener);
-        s_oStage.removeChild(_oGroup);
-        $(s_oMain).trigger("end_session");        
-        s_oGame.onExit();
-    };
-    
-    this._init(iCurWin);
-    
-    return this;
+    var oSprite = s_oSpriteLibrary.getSprite("msg_box");
+    _oBg = createBitmap(oSprite);
+    _oBg.x = CANVAS_WIDTH / 2;
+    _oBg.y = CANVAS_HEIGHT / 2;
+    _oBg.regX = oSprite.width / 2;
+    _oBg.regY = oSprite.height / 2;
+    _oGroup.addChild(_oBg);
+
+    _oMsgText = new CTLText(
+      _oGroup,
+      CANVAS_WIDTH / 2 - 250,
+      CANVAS_HEIGHT / 2,
+      500,
+      100,
+      50,
+      "center",
+      "#fff",
+      THIRD_FONT,
+      1,
+      0,
+      0,
+      " ",
+      true,
+      true,
+      true,
+      false
+    );
+  };
+
+  this.unload = function () {
+    _oGroup.off("click", _oListener);
+  };
+
+  this._initListener = function () {
+    _oListener = _oGroup.on("click", this._onExit);
+  };
+
+  this.show = function (oSprite) {
+    $(s_oMain).trigger("show_interlevel_ad");
+
+    _oMsgText.refreshText();
+
+    _oGroup.visible = true;
+
+    var oParent = this;
+    createjs.Tween.get(_oGroup)
+      .to({ alpha: 1 }, 500)
+      .call(function () {
+        oParent._initListener();
+      });
+  };
+
+  this._onExit = function () {
+    if (INSTANT_WHEEL_SETTINGS[iCurWin].redeem_link !== "") {
+      window.open(INSTANT_WHEEL_SETTINGS[iCurWin].redeem_link);
+    }
+
+    _oGroup.off("click", _oListener);
+    s_oStage.removeChild(_oGroup);
+    $(s_oMain).trigger("end_session");
+    s_oGame.onExit();
+  };
+
+  this._init(iCurWin);
+
+  return this;
 }
