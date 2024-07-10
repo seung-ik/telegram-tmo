70461a607abff0b9bdca68a942987663fda64571
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-07-10 10:51:37 +0900

    feat: 게임추가

--- src/tapten/index.tsx
+++ src/tapten/index.tsx
@@ -8,7 +8,7 @@ const TaptenIframe: React.FC = () => {
   return (
     <div style={{ width: "100%", height: "calc(100% - 20px)" }}>
       <iframe
-        src="/telegram-tmo/tapten/index.html"
+        src="/telegram-tmo/jump/index.html"
         title="HTML5 Game"
         style={{ border: "none", width: "100%", height: "100%" }}
       ></iframe>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "src/game/**/*", // 복사할 파일들의 경로
+    //       dest: "public/game-files", // 복사된 파일들이 저장될 목적지
+    //     },
+    //   ],
+    // }),
+  ],
+
   base: "/telegram-tmo/",
   build: {
     rollupOptions: {},
