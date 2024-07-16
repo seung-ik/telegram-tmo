5caf6d73f495283f2ef17188e95a305f020cbe8f
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-07-16 16:33:24 +0900

    test: plus_new game

--- src/plus/PlusIframe.tsx
+++ src/plus/PlusIframe.tsx
@@ -69,7 +69,7 @@ const PlusIframe: React.FC = () => {
   return (
     <div style={{ width: "100%", height: "calc(100% - 20px)" }}>
       <iframe
-        src="/telegram-tmo/game/plus/index.html"
+        src="/telegram-tmo/game/plus_new/index.html"
         title="HTML5 Game"
         style={{ border: "none", width: "100%", height: "100%" }}
       ></iframe>
