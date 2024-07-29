2e9c559014cac737b6cec4a375b9ec770cea6b26
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-07-29 14:35:04 +0900

    fix: delete unused code

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,28 @@ import Router, { Paths } from "./Router";
 import { useEffect } from "react";
 // import { request } from "@telegram-apps/sdk";
 import { initSwipeBehavior } from "@telegram-apps/sdk";
-import { mockTelegramEnv, parseInitData, request } from "@telegram-apps/sdk";
+import { request } from "@telegram-apps/sdk";
+// import { mockTelegramEnv, parseInitData } from "@telegram-apps/sdk";
 
-const initDataRaw = new URLSearchParams([
-  [
-    "user",
-    JSON.stringify({
-      id: 99281932,
-      first_name: "Andrew",
-      last_name: "Rogue",
-      username: "rogue",
-      language_code: "en",
-      is_premium: true,
-      allows_write_to_pm: true,
-    }),
-  ],
-  ["hash", "89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31"],
-  ["auth_date", "1716922846"],
-  ["start_param", "debug"],
-  ["chat_type", "sender"],
-  ["chat_instance", "8428209589180549439"],
-]).toString();
+// const initDataRaw = new URLSearchParams([
+//   [
+//     "user",
+//     JSON.stringify({
+//       id: 99281932,
+//       first_name: "Andrew",
+//       last_name: "Rogue",
+//       username: "rogue",
+//       language_code: "en",
+//       is_premium: true,
+//       allows_write_to_pm: true,
+//     }),
+//   ],
+//   ["hash", "89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31"],
+//   ["auth_date", "1716922846"],
+//   ["start_param", "debug"],
+//   ["chat_type", "sender"],
+//   ["chat_instance", "8428209589180549439"],
+// ]).toString();
 // mockTelegramEnv({
 //   themeParams: {
 //     accentTextColor: "#6ab2f2",
