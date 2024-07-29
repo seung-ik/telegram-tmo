a279a3ca3e0d19f74adcd17356a1e2a46d6f0d71
오승익 <seungik@oseung-ig-ui-MacBookPro.local>
2024-07-29 14:31:35 +0900

    delete env mock

--- src/App.tsx
+++ src/App.tsx
@@ -26,27 +26,27 @@ const initDataRaw = new URLSearchParams([
   ["chat_type", "sender"],
   ["chat_instance", "8428209589180549439"],
 ]).toString();
-mockTelegramEnv({
-  themeParams: {
-    accentTextColor: "#6ab2f2",
-    bgColor: "#17212b",
-    buttonColor: "#5288c1",
-    buttonTextColor: "#ffffff",
-    destructiveTextColor: "#ec3942",
-    headerBgColor: "#17212b",
-    hintColor: "#708499",
-    linkColor: "#6ab3f3",
-    secondaryBgColor: "#232e3c",
-    sectionBgColor: "#17212b",
-    sectionHeaderTextColor: "#6ab3f3",
-    subtitleTextColor: "#708499",
-    textColor: "#f5f5f5",
-  },
-  initData: parseInitData(initDataRaw),
-  initDataRaw,
-  version: "7.7",
-  platform: "tdesktop",
-});
+// mockTelegramEnv({
+//   themeParams: {
+//     accentTextColor: "#6ab2f2",
+//     bgColor: "#17212b",
+//     buttonColor: "#5288c1",
+//     buttonTextColor: "#ffffff",
+//     destructiveTextColor: "#ec3942",
+//     headerBgColor: "#17212b",
+//     hintColor: "#708499",
+//     linkColor: "#6ab3f3",
+//     secondaryBgColor: "#232e3c",
+//     sectionBgColor: "#17212b",
+//     sectionHeaderTextColor: "#6ab3f3",
+//     subtitleTextColor: "#708499",
+//     textColor: "#f5f5f5",
+//   },
+//   initData: parseInitData(initDataRaw),
+//   initDataRaw,
+//   version: "7.7",
+//   platform: "tdesktop",
+// });
 
 function App() {
   const navigate = useNavigate();
